d27046d3654cbd48e21ec862eb6ca5ded235a475
arbind <arbind@ubuntu.(none)>
2015-01-06 18:53:45 +0530

    User registration data display

--- app/views/protected/standardUser/user_page_1.blade.php
+++ app/views/protected/standardUser/user_page_1.blade.php
@@ -9,9 +9,18 @@
 	@endif
 
 	@if (Sentry::check())
-		<p>{{ "Welcome, " . Sentry::getUser()->first_name }}</p>
+		<p>{{ "Welcome, " . Sentry::getUser()->name }}</p>
 	@endif
 
-	<p>This is for standard users only!</p>
+	<p>This is for stars-users only!</p>
+	{{ HTML::image(Sentry::getUser()->profileimage , 'profile picture', array('class' => 'img-circle pull-right')) }}
+	<h3>Title</h3><p>{{Sentry::getUser()->title}}</p>
+	<h3>Joined Date</h3><p>{{Sentry::getUser()->created_at->toFormattedDateString();}} </p>
+	<h3>Address</h3><p>{{Sentry::getUser()->city}},{{ Sentry::getUser()->country}}</p>
+	
+@if(Sentry::check())
 
+		{{ link_to_route('profiles.edit', 'Edit Your userProfile', Sentry::getUser()->id, ['class' => 'btn btn-primary']) }}
+		
+	@endif
 @stop
